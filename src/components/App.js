260b9109a5a61cea98b2eaf267b6eb73e2e4079a
Amit Kashyap <mrkashyapamit@gamil.com>
2024-12-26 16:34:20 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,32 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   let [todo, setTodo] = useState("");
+    let [todolist, setTodoList] = useState([]);
+  
+    function submitHandler() {
+      setTodoList([...todolist,todo]);
+      setTodo("");
+    }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Todo-List App</h1>
+      <input
+        type="text"
+        value={todo} 
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={submitHandler}>Add Todo</button>
+      {
+        todolist.map((item, index) => (
+          <div key={index}>
+            {item}
+            <button onClick={() => setTodoList(todolist.filter((_, i) => i!== index))}>
+              Delete
+            </button>
+          </div>
+        ))
+      }
     </div>
   )
 }
